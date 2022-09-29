61676927110f02bbcd66e57d2bc7e3840c8d8ddf
Guilherme Pinho <guilhermemoraispinho@gmail.com>
2022-09-29 20:49:47 -0300

    Requisito 5 feito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const listOfAnimals = () => {
+  const animais = {};
+  species.forEach((element) => {
+    animais[element.name] = element.residents.length;
+  });
+  return animais;
+};
+
+const numeroDeAnimais = (animal) => {
+  const animais = species.find((tipo) => tipo.name === animal.specie).residents;
+  if (animal.sex) {
+    return animais.filter((specie) => specie.sex === animal.sex).length;
+  }
+  return animais.length;
+};
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return listOfAnimals();
+  }
+  return numeroDeAnimais(animal);
 }
 
 module.exports = countAnimals;
