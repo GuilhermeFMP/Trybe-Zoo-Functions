742cb8108b199097ac29fa928fb4a0e1625a209f
Guilherme Pinho <guilhermemoraispinho@gmail.com>
2022-09-29 21:23:53 -0300

    Requisito 6 e 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,15 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Testando em 80%', () => {
+    expect(handlerElephants('count')).toEqual(4);
+    expect(handlerElephants('names')).toContain('Jefferson');
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+  it('Testando em 90%', () => {
+    expect(handlerElephants('location')).toEqual('NW');
+    expect(handlerElephants('popularity')).toBeGreaterThanOrEqual(5);
+    expect(handlerElephants('availability')).not.toContain('Monday');
+    expect(handlerElephants()).toBeUndefined();
+  });
+});
